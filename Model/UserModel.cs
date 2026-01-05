aacc5a779bbd79ff066a076f51d72c92381be190
AbdelmalekYamouni <malek_yamouni@gmx.de>
2026-01-05 18:52:20 +0100

    Initial commit - UserManagement WPF application

---
+++ Model/UserModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.Model
+{
+
+    public enum UserStatus
+    {
+        Active,
+        Inactive,
+        Locked
+    }
+    public class UserModel : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        private UserStatus _userStatus;
+        public UserStatus Status
+        {
+            get => _userStatus; set
+            {
+                if (_userStatus != value)
+                {
+                    _userStatus = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public UserModel(int id, string firstname, string lastname, string email)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ViewModel/ViewModelMainWindow.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using UserManagement.Model;
+using UserManagement.Relay;
+using UserManagement.ViewModel;
+
+namespace UserManagement.ViewModel
+{
+    public class ViewModelMainWindow : ViewBase
+    {
+
+        private ObservableCollection<UserModel> _users;
+        private UserModel _selectedUser;
+        private int _userId;
+        private string _newFirstName;
+        private string _newLastName;
+        private string _newEmail;
+
+        public RelayCommand AddNewUserCommand => new RelayCommand(execute => AddNewUser(), canExecute => !string.IsNullOrWhiteSpace(NewFirstName) && !string.IsNullOrWhiteSpace(NewLastName) && !string.IsNullOrWhiteSpace(NewEmail));
+        public RelayCommand MarkAsInactiveCommand => new RelayCommand(execute => MarkAsInactive());
+        public RelayCommand MarkAsActiveCommand => new RelayCommand(execute => MarkAsActive());
+        public RelayCommand MarkAsLockedCommand => new RelayCommand(execute => MarkAsLocked());
+
+
+        public UserModel SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                if (_selectedUser != value)
+                {
+                    _selectedUser = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ViewModelMainWindow()
+        {
+            _users = new ObservableCollection<UserModel> {
+                    new UserModel(1, "John", "Doe", "john_doe@gmx.de"),
+                    new UserModel(2, "Malek", "Yamouni", "malek_yamouni@gmx.de"),
+                    new UserModel(3, "Jean", "Zimmermann", "jean_Zimmermann@gmx.de")
+            };
+        }
+
+        public ObservableCollection<UserModel> UserCollection
+        {
+            get { return _users; }
+            set
+            {
+                if (_users != value)
+                {
+                    _users = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string NewFirstName
+        {
+            get { return _newFirstName; }
+            set
+            {
+                if (_newFirstName != value)
+                {
+                    _newFirstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string NewLastName
+        {
+            get { return _newLastName; }
+            set
+            {
+                if (_newLastName != value)
+                {
+                    _newLastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string NewEmail
+        {
+            get { return _newEmail; }
+            set
+            {
+                if (_newEmail != value)
+                {
+                    _newEmail = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int GetMaxUserId()
+        {
+            if (_users == null || _users.Count == 0)
+            {
+                return 0;
+            }
+
+            return _users.Max(u => u.Id);
+        }
+
+        public void AddNewUser()
+        {
+            if (string.IsNullOrWhiteSpace(NewFirstName) || string.IsNullOrWhiteSpace(NewLastName) || string.IsNullOrWhiteSpace(NewEmail))
+            {
+                MessageBox.Show("Please fill in all fields to add a new user.", "Input Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            int newId = GetMaxUserId() + 1;
+            var user = new UserModel(newId, NewFirstName, NewLastName, NewEmail);
+            NewUserMarkAsActive(user);
+            UserCollection.Add(user);
+
+            NewFirstName = string.Empty;
+            NewLastName = string.Empty;
+            NewEmail = string.Empty;
+        }
+
+        public UserStatus NewUserMarkAsActive(UserModel user)
+        {
+            if (user != null)
+            {
+                user.Status = UserStatus.Active;
+            }
+            return user.Status;
+        }
+
+        public UserStatus MarkAsActive()
+        {
+            if (_selectedUser != null)
+            {
+                _selectedUser.Status = UserStatus.Active;
+                return _selectedUser.Status;
+            }
+            else
+            {
+                MessageBox.Show("Please select a User", "Selection Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return UserStatus.Active;
+            }
+        }
+
+        public UserStatus MarkAsInactive()
+        {
+            if (_selectedUser != null)
+            {
+                _selectedUser.Status = UserStatus.Inactive;
+                return _selectedUser.Status;
+            }
+            else
+            {
+                MessageBox.Show("Please select a User", "Selection Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return UserStatus.Active;
+            }
+        }
+
+
+        public UserStatus MarkAsLocked()
+        {
+            if (_selectedUser != null)
+            {
+                _selectedUser.Status = UserStatus.Locked;
+                return _selectedUser.Status;
+            }
+            else
+            {
+                MessageBox.Show("Please select a User", "Selection Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return UserStatus.Active;
+            }
+        }
+    }
+}
